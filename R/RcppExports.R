7f5e4f797260f7f356a0bcf8c6d5bf84a920dad4
Xavier Didelot <xavier.didelot@gmail.com>
2017-08-30 12:26:11 +0100

    Update RcppExports.R

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -1,8 +1,8 @@
 
 mh_sample_ne1_2  <- function ( fwdne, fwdnco , tau, prop_sigma, dh , lterms ){
-	.Call( 'phyloma_mh_sample_ne1_2', PACKAGE = 'phylo.ma', fwdne, fwdnco, tau, prop_sigma, dh, lterms )
+	.Call( 'phyloma_mh_sample_ne1_2', PACKAGE = 'armasky', fwdne, fwdnco, tau, prop_sigma, dh, lterms )
 }
 
 mh_sample_ne2_2  <- function ( fwdne, fwdnco , tau, prop_sigma, dh, zxb, lterms ){
-	.Call( 'phyloma_mh_sample_ne2_2', PACKAGE = 'phylo.ma', fwdne, fwdnco, tau, prop_sigma, dh , zxb, lterms )
+	.Call( 'phyloma_mh_sample_ne2_2', PACKAGE = 'armasky', fwdne, fwdnco, tau, prop_sigma, dh , zxb, lterms )
 }
